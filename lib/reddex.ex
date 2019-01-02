a3572ab9d5cf69e3b6373666262c064845029ac2
Chris de Graaf <chrisadegraaf@gmail.com>
2019-01-02 10:59:47 -0600

    Initial commit

---
+++ lib/reddex.ex
@@ -0,0 +1,18 @@
+defmodule Reddex do
+  @moduledoc """
+  Documentation for Reddex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Reddex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/reddex/application.ex
@@ -0,0 +1,20 @@
+defmodule Reddex.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Reddex.Worker.start_link(arg)
+      # {Reddex.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Reddex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
